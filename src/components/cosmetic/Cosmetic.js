dacb5947215ef147be0ef862e0c41f4df440ac5f
NayemEhsan <naymakon@gmail.com>
2023-03-25 21:59:39 +0600

    3rd commit

--- src/components/cosmetic/Cosmetic.js
+++ src/components/cosmetic/Cosmetic.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './Cosmetic.css'
 const Cosmetic = (props) => {
     const {name,price,_id} = props.cosmetic;
+    const addToCart =(_id) =>{
+        console.log('item added',_id);
+    }
     return (
         <div className='cosmetic'>
            <h2>Buy This :{name}</h2>
            <p>Only For:${price}</p> 
            <p>id:{_id}</p>
+           <button onClick={() => addToCart(_id)}>Add TO Cart</button>
         </div>
     );
 };
